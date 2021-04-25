b6d89c23a040d8a405272aaf7ee4ec18b0132e83
Binghui Zhang <z767658538@gmail.com>
2021-04-24 19:28:42 -0500

    Update cnnmodel.py
    use GPU

--- cnnmodel.py
+++ cnnmodel.py
@@ -13,7 +13,7 @@ from torch.utils.data import DataLoader
 
 class CNNModel(nn.Module):
     
-    def __init__(self, vocab_size=1000, kernel_size=10, num_filter_maps=16, embed_size=100, dropout=0.5):
+    def __init__(self, vocab_size=5000, kernel_size=10, num_filter_maps=16, embed_size=100, dropout=0.5):
         super(CNNModel, self).__init__()
         print("initializing CNNMOdel")
         self.embed_size = embed_size
@@ -39,11 +39,11 @@ class CNNModel(nn.Module):
         xavier_uniform_(self.final.weight)
     
     def forward(self, text):
-        text = text.transpose(1,-1)
-        embedded = self.embed(text)
-        embedded = self.embed_drop(embedded)
-        embedded = embedded.transpose(1,-1)
-        conved = torch.tanh(self.conv(embedded))
+        text = text.transpose(1,-1).to(device)
+        embedded = self.embed(text).to(device)
+        embedded = self.embed_drop(embedded).to(device)
+        embedded = embedded.transpose(1,-1).to(device)
+        conved = torch.tanh(self.conv(embedded)).to(device)
         #attention = F.softmax(torch.matmul(self.u.weight, conved), dim=2) 
         #v = torch.matmul(attention, conved.transpose(1,-1))
         #y = torch.sigmoid(self.final(v)).squeeze() #this might need a transpose before it if dimensions dont work out.
@@ -64,19 +64,21 @@ class CNNModel(nn.Module):
 def eval(model, data):
     print("calling eval")
     model.eval()
-    y_pred = torch.LongTensor()
-    y_true = torch.LongTensor()
+    y_pred = torch.LongTensor().to(device)
+    y_true = torch.LongTensor().to(device)
     for x, y in data:
         #y_hat, attention = model(x)
+        x = x.to(device)
+        y = y.to(device)
         y_hat = model(x)
-        y_pred = torch.cat((y_pred, y_hat.detach().to('cpu')), dim=0)
-        y_true = torch.cat((y_true, y.detach().to('cpu')), dim=0)
-    return average_precision_score(y_true, y_pred)
+        y_pred = torch.cat((y_pred, y_hat.detach().to(device)), dim=0)
+        y_true = torch.cat((y_true, y.detach().to(device)), dim=0)
+    return average_precision_score(y_true.cpu(), y_pred.cpu())
 
 
 def train(model, train_data, test_data, n_epochs):
     print("starting train")
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    optimizer = optim.Adam(model.parameters(), lr=0.002)
     criterion = nn.BCELoss()
     for epoch in range(n_epochs):
         print("starting epoch ", epoch)
@@ -84,6 +86,8 @@ def train(model, train_data, test_data, n_epochs):
         model.train()
         train_loss = 0
         for x, y in train_data:
+            x = x.to(device)
+            y = y.to(device)
             optimizer.zero_grad()
             #y_hat, attention = model(x) # need to not get attention from return of forward
             y_hat = model(x) # adding this new invocation for plain CNN instead of the above which returned attention
@@ -101,8 +105,15 @@ def train(model, train_data, test_data, n_epochs):
               f"\tTime Taken: {end_time-start_time:.2f} seconds")
 
 model = CNNModel()
+if torch.cuda.is_available():  
+  dev = "cuda:0" 
+else:  
+  dev = "cpu"  
+device = torch.device(dev)  
+model.to(device)
+
 print("loading training dataset")
-train_ds = data.load_dataset("data/train.csv", vocab_size=1000)
+train_ds = data.load_dataset("data/train.csv", vocab_size=5000)
 #train_ds = cnndata.load_dataset("data/train.csv", vocab_size=1000)
 print("calling DataLoader for train dataset")
 train_loader = DataLoader(train_ds, batch_size=10, collate_fn=data.collate, shuffle=True)
@@ -114,4 +125,5 @@ print("calling Dataloader for test dataset")
 test_loader = DataLoader(test_ds, batch_size=10, collate_fn=data.collate, shuffle=False)
 #test_loader = DataLoader(test_ds, batch_size=10, collate_fn=cnndata.collate, shuffle=False)
 print("calling train on the model")
-train(model, train_loader, test_loader, 2)
+
+train(model, train_loader, test_loader, 20)
